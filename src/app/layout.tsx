3fb57012df18b175cc56b903ba99e150dceee26d
Ugochukwu Lopez Ubani <117824886+Lopdee@users.noreply.github.com>
2025-09-11 03:55:29 +0100

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import "./globals.css"
-import { ReactNode } from "react"
-import Link from "next/link"
+import "../../styles/globals.css";
+import { ReactNode } from "react";
+import Link from "next/link";
 
 export const metadata = {
   title: "Polling App",
